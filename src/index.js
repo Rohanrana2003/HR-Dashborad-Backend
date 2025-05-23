6109df48efb6559fab095927777823e5456635ce
Rohan rana <rohanrana33042@gmail.com>
2025-05-23 15:04:43 +0530

    Refactor upload static file path for AWS compatibility

--- src/index.js
+++ src/index.js
@@ -29,7 +29,8 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/uploads", express.static(path.join(__dirname, "../uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "../uploads")));  //FOR Render
+app.use("/api/uploads", express.static(path.join(__dirname, "../uploads"))); // FOR AWS
 
 const authRouter = require("./routes/auth");
 const candidateRouter = require("./routes/candidates");
